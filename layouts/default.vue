4d9782c1a7c30236b2d89bd3edf888854c77a0b5
Shamil Frontend <sh.alisultanov@maximatelecom.ru>
2019-06-29 21:48:00 +0300

    init nuxt app, Настройка приложения (nuxt + socket.io)

---
+++ layouts/default.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-app app>
+    <h1>Title</h1>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data() {
+    return {}
+  }
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-layout
+    column
+    justify-center
+    align-center
+  >
+  </v-layout>
+</template>
+
+<script>
+
+export default {
+  components: {
+  }
+}
+</script>
